9013594034967a060cc79308d1079ad32058b19f
YukeshKumaran <162685072+YukeshKumaran@users.noreply.github.com>
2024-07-20 15:25:58 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,7 +1,6 @@
 #Write a Python program to check if a string is a palindrome or not.
 
 char=input("Enter String:")
-print("Char:",char)
 if char== char[::-1]:
     print("yes It is palindrome")
 else:
